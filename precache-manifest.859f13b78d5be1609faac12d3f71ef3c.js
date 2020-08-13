1d2187726c60183ca0475238c0ece5c42cd2e1b8
root <root@DESKTOP-LC1EVA9.localdomain>
2020-08-13 22:37:41 +0800

    Updates

---
+++ precache-manifest.859f13b78d5be1609faac12d3f71ef3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46a3597e20ef5a20f0fc2f46a6f10c5",
+    "url": "/firesystems/index.html"
+  },
+  {
+    "revision": "5e354e93742ba72e2e7a",
+    "url": "/firesystems/static/css/main.34fbadb2.chunk.css"
+  },
+  {
+    "revision": "840c8b5d879a887bba85",
+    "url": "/firesystems/static/js/2.11bbca11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/firesystems/static/js/2.11bbca11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e354e93742ba72e2e7a",
+    "url": "/firesystems/static/js/main.ce303de2.chunk.js"
+  },
+  {
+    "revision": "d7b9c0c3f8c5d3fad41a",
+    "url": "/firesystems/static/js/runtime-main.bb5b0936.js"
+  }
+]);
